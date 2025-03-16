759f83147d45c0f36d3dccd9549405389dddcb49
satejh <satejhatalkar@gmail.com>
2025-03-16 12:31:52 +0530

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,22 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  site: {
+    url: 'https://physiotherapy.blogerpedia.com',
+    name: 'Physiotherapy in Mumbai',
+    description: 'Best Physiotherapy in Mumbai',
+    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
-})
 
+  modules: ['@nuxtjs/seo'],
+  
+})
